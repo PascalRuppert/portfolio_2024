5fe5e6602b1e8b251364411e0fa55e10a87be2b3
Pascal Ruppert <ixd.pascalruppert@gmail.com>
2023-05-24 16:09:21 +0900

    feat: creating case section of index page
    - creating CaseSection component
    - loading cases from content/case directory
    - creating CaseCard component

---
+++ components/CaseSection/CaseCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <NuxtLink :to="slug" class="col-span-4 sm:col-span-2 md:col-span-3 group hover:cursor-pointer">
+    <div class="overflow-hidden rounded aspect-w-4 aspect-h-3">
+      <img :src="image" :alt="image" class="object-cover object-center w-full h-full transition duration-150 ease-in-out group-hover:scale-105" />
+    </div>
+    <h3 class="mt-4 antialiased text-itemtitle font-jet text-typo-600 xl:text-xl-itemtitle text-shadow">
+      {{ title }}
+    </h3>
+    <p class="mt-2 antialiased text-body font-jet text-typo-500 xl:text-xl-body">
+      {{ description }}
+    </p>
+    <div class="inline-block mt-4 antialiased underline uppercase sm:no-underline underline-offset-[6px] text-btn text-typo-600 font-jet xl:text-xl-btn">
+      View project ->
+      <div class="w-full h-px mt-px bg-gradient-to-r from-link to-link sm:bg-no-repeat [background-position:0] [background-size:0] sm:motion-safe:transition-all sm:motion-safe:duration-150 sm:group-hover:[background-size:100%] sm:group-focus:[background-size:100%]"></div>
+    </div>
+  </NuxtLink>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ components/CaseSection/index.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <section class="relative px-6 mt-24 sm:px-12 xl:px-16 2xl:max-w-7xl 2xl:mx-auto 2xl:px-0 xl:mt-36">
+    <div class="grid w-full grid-cols-4 gap-x-4 sm:gap-x-6 md:grid-cols-6 xl:gap-x-8">
+      <hgroup class="col-span-2 mb-10 xl:mb-12">
+        <h3 class="antialiased uppercase text-section font-jet text-var-1 xl:text-xl-section">Work</h3>
+        <h2 class="mt-2 antialiased text-sectiontitle font-jet text-typo-600 xl:text-xl-sectiontitle text-shadow">Featured<br />projects</h2>
+      </hgroup>
+    </div>
+
+    <div class="grid w-full grid-cols-4 gap-x-4 gap-y-14 sm:gap-x-6 md:grid-cols-6 xl:gap-x-8">
+      <ContentQuery v-slot="{ data }" path="/case">
+        <CaseCard v-for="item of data" :key="item" :title="item.title" :description="item.description" :image="item.img" :slug="item._path" />
+      </ContentQuery>
+    </div>
+  </section>
+</template>
