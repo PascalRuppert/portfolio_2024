731befc96a9243fc3f8a4dc2cc8eda20b1546515
Pascal Ruppert <ixd.pascalruppert@gmail.com>
2023-06-12 23:12:29 +0900

    feat: updating and adding favicon files

--- app.vue
+++ app.vue
@@ -7,6 +7,32 @@ useHead({
   bodyAttrs: {
     class: 'bg-base-200 overscroll-none',
   },
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#04243d' },
+  ],
+  meta: [
+    { name: 'msapplication-TileColor', content: '#2b5797' },
+    { name: 'theme-color', content: '#021626' },
+  ],
 });
 </script>
 
