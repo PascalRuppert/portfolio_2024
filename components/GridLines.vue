1c1b0c9095b4b3d2157ceedc1ecdfe5c8e81e47c
Pascal Ruppert <ixd.pascalruppert@gmail.com>
2023-06-15 21:17:19 +0900

    fix: optimizing gridlines
    - preventing user selection
    - reducing opacity on mobile

--- components/GridLines.vue
+++ components/GridLines.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute w-screen h-full" aria-hidden="true">
+  <div class="absolute w-screen h-full select-none opacity-60 sm:opacity-100" aria-hidden="true">
     <div class="w-full h-full px-6 sm:px-12 xl:px-16 2xl:max-w-7xl 2xl:mx-auto 2xl:px-0">
       <div class="grid w-full h-full grid-cols-4 gap-x-4 sm:gap-x-6 md:grid-cols-6 xl:gap-x-8">
         <div class="h-full border-lines border-x-[0.5px]"></div>
