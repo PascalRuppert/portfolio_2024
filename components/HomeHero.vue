5c964fc30a566bff4bf3fa759fde23ccb7a093db
Pascal Ruppert <ixd.pascalruppert@gmail.com>
2023-06-16 18:06:51 +0900

    fix: hiding overflow of memoji video container
    Preventing transparent video background from overlapping the hero title

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -2,7 +2,7 @@
   <section class="relative px-6 sm:px-12 xl:px-16 2xl:max-w-7xl 2xl:mx-auto 2xl:px-0 group home-hero">
     <div class="grid w-full grid-cols-4 gap-x-4 sm:gap-x-6 md:grid-cols-6 xl:gap-x-8">
       <div class="col-span-4 sm:col-span-3 md:col-span-4">
-        <div class="h-14 w-14 xl:h-18 xl:w-18 memoji">
+        <div class="overflow-hidden h-14 w-14 xl:h-18 xl:w-18 memoji">
           <video src="../assets/memoji.mov" loop muted playsinline data-src="'../assets/memoji.mov" poster="../assets/memoji_poster.png" class="h-[118%] scale-[1.9]" />
         </div>
         <ContentDoc path="/home_hero" class="mt-3 xl:mt-4" />
